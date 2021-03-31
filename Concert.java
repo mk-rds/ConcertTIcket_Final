ac75d431231f999600d9bafb3c8b7124af962904
KennethMordon <79129049+KennethMordon@users.noreply.github.com>
2021-03-31 19:10:32 +0800

    Update Concert.java

--- Concert.java
+++ Concert.java
@@ -3,26 +3,37 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package demo;
+package FinalAssignment;
+
 
 /**
  *
- * @author User
+ * @author Gigabyte
  */
 public class Concert {
+
     private String conName;
     private String conTime;
-    
-    
-    public Concert(){
-        
+    private String conDesc;
+    private String conDest;
+    private String conArtist;
+
+    public Concert() {
+
     }
 
-    public Concert(String conName, String conTime) {
+    public Concert(String conName, String conTime, String conDesc, String conDest, String conArtist) {
         this.conName = conName;
         this.conTime = conTime;
+        this.conDesc = conDesc;
+        this.conDest = conDest;
+        this.conArtist = conArtist;
     }
 
+    public Concert(String conName){
+        this.conName = conName;
+    }
+    
     public String getConName() {
         return conName;
     }
@@ -39,11 +50,36 @@ public class Concert {
         this.conTime = conTime;
     }
 
+    public String getConDesc() {
+        return conDesc;
+    }
+
+    public void setConDesc(String conDesc) {
+        this.conDesc = conDesc;
+    }
+
+    public String getConDest() {
+        return conDest;
+    }
+
+    public void setConDest(String conDest) {
+        this.conDest = conDest;
+    }
+
+    public String getConArtist() {
+        return conArtist;
+    }
+
+    public void setConArtist(String conArtist) {
+        this.conArtist = conArtist;
+    }
+
+
     @Override
     public String toString() {
-        return "Concert{" + "conName=" + conName + ", conTime=" + conTime + '}';
+        return "\nConcert Name : " + conName + "\nConcert Artist : " + conArtist + "\nConcert Time: " + conTime + "\nConcert Destination : " + conDest + "\nConcert Description : "
+                + conDesc;
     }
-    
-   
-    
-}
+
+} 
+
