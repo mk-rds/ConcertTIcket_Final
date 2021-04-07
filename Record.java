43f5c90d859dcd6b693d56bed6db640816a49032
LeeHanShen <77600251+LeeHanShen@users.noreply.github.com>
2021-04-07 15:45:30 +0800

    Add files via upload

---
+++ Record.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo3;
+
+/**
+ *
+ * @author User
+ */
+public class Record {
+    private user user;
+    private TicketSystem TicketSystem;
+
+    public Record(user user,TicketSystem TicketSystem) {
+        this.user = user;
+        this.TicketSystem = TicketSystem;
+    }
+
+    @Override
+    public String toString() {
+        return "Record{" + "user=" + user + ", TicketSystem=" + TicketSystem + '}';
+    }
+
+    
+    
+    
+}
