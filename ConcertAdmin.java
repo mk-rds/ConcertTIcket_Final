22d643506b3ad646bcd19cf554b9ca939efc7d9c
LeeHanShen <77600251+LeeHanShen@users.noreply.github.com>
2021-04-07 16:22:15 +0800

    Update ConcertAdmin.java

--- ConcertAdmin.java
+++ ConcertAdmin.java
@@ -38,5 +38,16 @@ public class ConcertAdmin extends Staff{
         return this.getBasicSalary()*CommissionRate + this.getBasicSalary();
     }
     
-    
+    public static void ConcertDetails(Concert Concert , int subtotal,ticket ticket){
+        System.out.println("Concert name : " + Concert.getConName());
+        System.out.println("Concert time : " + Concert.getConTime());
+        System.out.println("TicketStock : \n" + "CAT1 : "+ ticket.getCAT1()+ 
+                                                " CAT2 : " + ticket.getCAT2()+
+                                                " CAT3 : " + ticket.getCAT3()+
+                                                " CAT4 : " + ticket.getCAT4()+
+                                                " CAT5 : " + ticket.getCAT5());
+        
+        System.out.println("total revenue : " + subtotal);
+        
+    }
 }
