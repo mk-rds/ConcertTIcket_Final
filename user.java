4acd816019c668469d2e71ebdc5d90a88f9b60b9
LeeHanShen <77600251+LeeHanShen@users.noreply.github.com>
2021-04-07 15:44:17 +0800

    Add files via upload

---
+++ user.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo3;
+
+/**
+ *
+ * @author User
+ */
+public class user {
+    private String name = "hanshen";
+    private String password;
+
+    public user() {
+    }
+
+    
+    public user(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "name=" + name + ", password=" + password + '}';
+    }
+}
