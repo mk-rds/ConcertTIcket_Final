76455cd924791bf5070beb1435f4ee01d8632098
LeeHanShen <77600251+LeeHanShen@users.noreply.github.com>
2021-04-07 15:18:05 +0800

    Update ConcertStocker.java

--- ConcertStocker.java
+++ ConcertStocker.java
@@ -48,5 +48,28 @@ public class ConcertStocker extends Staff{
         
     }
     
+    private double allowanceRate;
+
+    public ConcertStocker(String name, int YearJoined, double basicSalary,double allowanceRate) {
+        super(name, YearJoined, basicSalary);
+        this.allowanceRate = allowanceRate;
+    }
+
+    public double getAllowanceRate() {
+        return allowanceRate;
+    }
+
+    public void setAllowanceRate(double allowanceRate) {
+        this.allowanceRate = allowanceRate;
+    }
+
+    @Override
+    public String toString() {
+        return "ConcertStocker{" + "allowanceRate=" + allowanceRate + '}';
+    }
     
+    @Override
+    public double CalcSalary(){
+        return this.getBasicSalary()*allowanceRate + this.getBasicSalary();
+    }
 }
